b8b2744da2a46abc8bc94921e9c1c25946022b39
liuliu_global <liuliu_global@qiutianji.com>
2020-04-03 17:47:16 +0800

    又进步一点点

--- python_simple/python_01.py
+++ python_simple/python_01.py
@@ -60,4 +60,90 @@ else:
 #原因 ：tab和空格没对齐的问题
 #Python 的代码块中必须使用相同数目的行首缩进空格数。建议每个缩进层次使用 单个制表符 或 两个空格 或 四个空格 , 切记不能混用
 
-print("to hello")
+"""
+多行语句
+Python 通常是一行写完一条语句，但如果语句很长，我们可以使用反斜杠(\)来实现多行语句，例如：
+"""
+total = "item_one" +"\t"+ \
+        "item_two" +"\t"+ \
+        "item_three"
+#print(total)
+#在 [], {}, 或 () 中的多行语句，不需要使用反斜杠(\)，例如：
+total1 = ['item_one', 'item_two', 'item_three',
+        'item_four', 'item_five']
+
+#print(total1)
+
+total2 = {'item_one', 'item_two', 'item_three',
+        'item_four', 'item_five'}
+
+#print(total2)
+
+
+total3 = ('item_one', 'item_two', 'item_three',
+        'item_four', 'item_five')
+
+#print(total3)
+
+
+'''
+数据类型：python中数字有四种类型：整数、布尔型、浮点数和复数。
+    int (整数), 如 1, 只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。
+    bool (布尔), 如 True。
+    float (浮点数), 如 1.23、3E-2
+    complex (复数), 如 1 + 2j、 1.1 + 2.2j
+'''
+
+#字符串(String)
+#    ①：python中单引号和双引号使用完全相同。
+#    ②：使用三引号('''或""")可以指定一个多行字符串。
+#    ③：转义符 '\'
+#    ④：反斜杠可以用来转义，使用r可以让反斜杠不发生转义。。 如 r"this is a line with \n" 则\n会显示，并不是换行。
+#    ⑤：按字面意义级联字符串，如"this " "is " "string"会被自动转换为this is string。
+#    ⑥：字符串可以用 + 运算符连接在一起，用 * 运算符重复。
+#    ⑦：Python 中的字符串有两种索引方式，从左往右以 0 开始，从右往左以 -1 开始。
+#    ⑧：Python中的字符串不能改变。
+#    ⑨：Python 没有单独的字符类型，一个字符就是长度为 1 的字符串。
+#    ⑩：字符串的截取的语法格式如下：变量[头下标:尾下标:步长]
+
+
+#等待用户输入
+'''
+import sys #这里实现了空行 下面说空行的作用
+
+print (sys.argv[0])
+price =input("price:[eg, 10,20]\n")
+a=price.split(',')
+print("price:"+price)
+'''
+
+"""
+空行[空行也是程序代码的一部分]
+函数之间或类的方法之间用空行分隔，表示一段新的代码的开始。类和函数入口之间也用一行空行分隔，以突出函数入口的开始。
+空行与代码缩进不同，空行并不是Python语法的一部分。书写时不插入空行，Python解释器运行也不会出错。但是空行的作用在于分隔两段不同功能或含义的代码，便于日后代码的维护或重构。
+"""
+
+
+#同一行显示多条语句[语句之间使用分号(;)分割]
+#import sys; x = 'python to hello'; sys.stdout.write(x + '\n')  #几条语句写在一行
+
+
+#代码组 [多个语句构成]
+"""
+缩进相同的一组语句构成一个代码块，我们称之代码组。
+像if、while、def和class这样的复合语句，首行以关键字开始，以冒号( : )结束，该行之后的一行或多行代码构成代码组。
+我们将首行及后面的代码组称为一个子句(clause)。
+"""
+import sys
+
+#print (sys.argv[0])
+int price=0
+price =input("请输入值：")
+
+
+if (price<10):
+    print("我是小于10的数字")
+elif (price==10):
+    print("我刚好是10") 
+else:
+    price("我是大于10的数字")
